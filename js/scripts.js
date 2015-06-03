b59f2e1d0753b81fddd9bf871ed8c3223898d23c
Ian MacDonald & Brent Bailey <student@epicodus.com>
2015-06-03 11:26:40 -0700

    Add all of the prototypes. Board can tell if game is won.

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,92 @@ $( document ).ready(function() {
 });
 
 //raw js
-var foo = function(bar) {
-  return false;
+function Player(mark) {
+  mark = mark.toUpperCase();
+  if (mark === "X"){
+    this.mark = "X"
+  } else if (mark === "O"){
+    this.mark = "O"
+  } else {
+    this.mark = null
+  };
+};
+
+function Space(coordinates, mark) {
+  this.coordinates = coordinates;
+  this.xCoordinate = coordinates[0];
+  this.yCoordinate = coordinates[1];
+};
+
+Space.prototype.mark = function(player) {
+  var mark = player.mark;
+  this.player = player
+  this.checkMark = player.mark;
+};
+
+function Board() {
+  this.spaces = [];
+  for (var y = 1; y <= 3; y++) {
+    for(var x = 1; x <= 3; x++) {
+      this.spaces.push(new Space([x, y], null));
+    };
+  };
+};
+
+Board.prototype.findSpace = function(coordinates) {
+  var length = this.spaces.length;
+  for (var i = 0; i < length; i++) {
+    if (this.spaces[i].coordinates[0] === coordinates[0] && this.spaces[i].coordinates[1] === coordinates[1]) {
+      return this.spaces[i];
+    }
+  }
+};
+
+Board.prototype.gameOver = function(player) {
+  var playerMark = player.mark;
+  var win = false, horizontalWin = false, verticalWin = false, positiveDiagWin = false, negativeDiagWin = false;
+  var horizontalMarks = [], verticalMarks = [], positiveDiagMarks = [], negativeDiagMarks = [];
+
+  for(var space = 0; space < 9; space++) {
+    if (playerMark = this.spaces[0].checkMark) {
+      for(var horizontal = -2; horizontal <= 2; horizontal++) {
+        if (!horizontalWin) {
+          horizontalWin = checkSpaces(this, horizontal, horizontalMarks, playerMark);
+        }
+      }
+      for(var vertical = -6; vertical <= 6; vertical += 3) {
+        if (!verticalWin) {
+          verticalWin = checkSpaces(this, vertical, verticalMarks, playerMark);
+        }
+      }
+      for(var positiveDiag = -8; positiveDiag <= 8; positiveDiag += 4) {
+        if (!positiveDiagWin) {
+          positiveDiagWin = checkSpaces(this, positiveDiag, positiveDiagMarks, playerMark);
+        }
+      }
+      for(var negativeDiag = -6; negativeDiag <= 6; negativeDiag += 2) {
+        if (!negativeDiagWin) {
+          negativeDiagWin = checkSpaces(this, negativeDiag, negativeDiagMarks, playerMark);
+        }
+      }
+    }
+  };
+
+  if (horizontalWin || verticalWin || positiveDiagWin || negativeDiagWin) {
+    win = true;
+  }
+
+  return win;
+};
+
+function checkSpaces(board, increment, marks, mark) {
+  // var marks = [];
+  var win = false;
+  if (typeof board.spaces[increment] != "undefined" && board.spaces[increment].checkMark === mark) {
+    marks.push(board.spaces[increment].checkMark);
+  }
+  if (marks.length === 3) {
+    win = true;
+  }
+  return win;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,58 @@
-describe('foo', function() {
-  it("is some function or expectation", function() {
-    expect(foo('bar')).to.equal(false);
+describe('Player', function() {
+  describe('mark', function() {
+    it("returns the player's mark", function() {
+      var player = new Player("X");
+      expect(player.mark).to.equal("X");
+    });
+  });
+});
+
+describe('Space', function(){
+  it("knows its coordinates", function() {
+    var testSpace = new Space([1, 1], null)
+    expect(testSpace.coordinates).to.eql([1, 1])
+  });
+
+  it("can be marked X or O", function(){
+    var testSpace = new Space([1, 1], null)
+    var player = new Player("X");
+    testSpace.mark(player)
+    expect(testSpace.checkMark).to.equal("X")
+  });
+});
+
+describe('Board', function() {
+  it("should be created with 9 spaces", function(){
+    var board = new Board();
+    expect(board.spaces.length).to.eql(9)
+  });
+  it("should return a specific space", function(){
+    var board = new Board();
+    var player = new Player("X");
+    board.spaces[1].mark(player);
+    expect(board.spaces[1].checkMark).to.eql("X")
+    expect(board.spaces[1].coordinates).to.eql([2, 1])
+  });
+  it("should find a space by coordinates", function() {
+    var board = new Board();
+    expect(board.findSpace([2, 1])).to.eql(board.spaces[1]);
+  });
+  it("should be able to tell if there are three in a row marked by the same player", function(){
+    var board = new Board();
+    var player = new Player("X");
+    board.spaces[0].mark(player);
+    board.spaces[1].mark(player);
+    board.spaces[2].mark(player);
+    expect(board.gameOver(player)).to.eql(true);
+  });
+});
+
+describe('Game', function() {
+  it("should create 2 players and a board", function(){
+    var game = new Game();
+    expect(game.players.length).to.eql(2)
+    expect(game.player1.mark).to.eql("X")
+    expect(game.player2.mark).to.eql("O")
+    expect(game.board.spaces.length).to.eql(9)
   });
 });
